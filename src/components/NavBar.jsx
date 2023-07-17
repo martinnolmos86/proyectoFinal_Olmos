e1a7dffd6d9feb74dfd8b6c908491203408825a8
Martin Olmos <martinnolmos@gmail.com>
2023-07-17 16:45:52 -0300

    logo agregado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import CartWidget from "./CartWidget";
+import Assets from "../assets/logo.png";
 
 const NavBar = () => {
   return (
     <>
       <StyledNav>
-        <h3>logo</h3>
+        <img src={Assets} alt="Logo" />
         <div>
           <a href="#">Producto</a>
           <a href="#">Contacto</a>
@@ -27,13 +28,15 @@ const StyledNav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 1.4rem;
-
-  h3 {
-    color: white;
+  padding: 1.2rem;
+  img {
+    height: 80px;
+    width: 120px;
+    margin-left: 1.5rem;
   }
   div {
     a {
+      font-size: 1.1rem;
       color: white;
       padding: 1rem;
       text-decoration: none;
